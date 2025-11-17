3455b401bcd4e2c34e9e8c45988d9cba9c873005
coolmusma <youth.cool.musma@gmail.com>
2025-11-17 18:02:05 +0900

    first commit

---
+++ codes/05_ifs.py
@@ -0,0 +1,16 @@
+first = 4
+
+# if first >= 5 :
+#     print("welcome to Kang min-joon")
+#     print("it's me")
+
+second = 6
+
+first = int(input("input first number:"))
+second = int(input("input second number: "))
+
+if first >= second :
+    print("welcome to Kang min-joon")
+    print("it's me")
+
+print("End of program.")
